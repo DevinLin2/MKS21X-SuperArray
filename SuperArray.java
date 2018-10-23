d5f6e46fcf5b7653b677edb8211df785585b7c12
VitaminOh <dlin01@stuy.edu>
2018-10-23 11:07:02 -0400

    finished get function

--- SuperArray.java
+++ SuperArray.java
@@ -5,4 +5,37 @@ public class SuperArray {
     data = new String[10];
     size = 0;
   }
+  public void clear(){
+    size = 0;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean isEmpty(){
+    return size() == 0;
+  }
+  public boolean add(String value){ //(This must be edited later such that: If the capacity is full, you must resize() first.)
+    data[size()] = value;
+  }
+  public String toString(){
+    String ans = "[";
+    for (int i = 0; i < size(); i++){
+      ans += get(i);
+      if (i + 1 != size()){
+        ans += ","
+      }
+    }
+    return ans;
+  }
+  public String toStringDebug(){
+    return "";
+  }
+  public String get(int index){
+    if (index < 0 || index >= size()){
+      return null;
+      System.out.println("Error: index out of range")
+    }else{
+      return data[index];
+    }
+  }
 }
